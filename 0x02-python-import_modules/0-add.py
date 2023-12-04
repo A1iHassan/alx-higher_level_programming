d3856dde4068b4b0fc4507ab17ef7587cb33fbb5
ALI <ali012wkout@gmail.com>
2023-12-04 18:36:57 +0200

    -

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -3,4 +3,4 @@ from add_0.py import add
 if __name__ == "__main__":
     a = 1
     b = 2
-    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+    print("{:d} + {:d} = {:d}".format(a, b, add(a = 1, b = 2)))
