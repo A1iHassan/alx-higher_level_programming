4ba06bc42d5240d21f9f30e3ad9169bbde8d5953
ALI <ali012wkout@gmail.com>
2023-12-04 20:14:43 +0200

    task 2 answer

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,7 +3,7 @@ if __name__ == "__main__":
     from sys import argv
     x = len(argv)
     y = "argument" if x == 2 else "arguments"
-    print("{} {}{}".format(x - 1, "." if x == 1 else ":"))
+    print("{} {}{}".format(x - 1, y, "." if x == 1 else ":"))
     if x > 1:
         for i in range(1, x):
             print("{}: {}".format(i, argv[i]))
