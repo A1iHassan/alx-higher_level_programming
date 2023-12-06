2387ac32018e4a089a1bfd2f333068965d8a1b5a
ALI <ali012wkout@gmail.com>
2023-12-06 20:24:47 +0200

    -

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    new = [[i[x]**2 for x in range(len(i) - 1)] for i in matrix]
+    new = [[i[x]**2 for x in range(len(i))] for i in matrix]
+    return new
