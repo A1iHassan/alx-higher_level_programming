947cab9350defa2e9a3f63ca4873fbdf34107df6
ALI <ali012wkout@gmail.com>
2023-12-19 14:48:25 +0200

    task 7 edit

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -6,6 +6,6 @@ def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
         return True
-    except (ValueError, TypeError) as error:
+    except Exception as error:
         sys.stderr("Exception: {}".format(error))
         return False
