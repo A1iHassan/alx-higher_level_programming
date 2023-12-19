090a3ede437ee20d7fb1f97e86cd9427ffe4dfea
ALI <ali012wkout@gmail.com>
2023-12-19 12:32:03 +0200

    task 0 edit

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -8,4 +8,7 @@ def safe_print_list(my_list=[], x=0):
         return i
     else:
         print()
-        return i + 1
+        if x == 0:
+            return x
+        else:
+            return i + 1
