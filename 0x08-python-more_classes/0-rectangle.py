03a7b83efb837d9a2e79c7532da2972a7480a82b
ALI <ali012wkout@gmail.com>
2024-01-29 21:38:44 +0200

    created the directory + task 0 answer

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""a module for the Rectangle class"""
+
+
+class Rectangle:
+    """a class for the Rectangle data type that contains all of the attributes of a rectangle"""
+
+    pass
