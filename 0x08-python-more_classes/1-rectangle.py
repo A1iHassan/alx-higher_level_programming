ba008f23d3d7e52552bb81ed43c4f9c0f1d91bbb
ALI <ali012wkout@gmail.com>
2024-01-29 22:01:22 +0200

    task 1 edit

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -24,3 +24,41 @@ class Rectangle:
         else:
             self.__width = width
             self.__height = height
+    
+    @property
+    def width(self):
+        """a getter property for the width attribute"""
+        return self.__width
+    
+    @width.setter
+    def width(self, value):
+        """a setter property for the width attribute
+
+        Args:
+            value: width to be set
+        """
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = value
+
+    @property
+    def height(self):
+        """a getter property for the height attribute"""
+        return self.__height
+    
+    @height.setter
+    def height(self, value):
+        """a setter property for the height attribute
+
+        Args:
+            value: height to be set
+        """
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = value
