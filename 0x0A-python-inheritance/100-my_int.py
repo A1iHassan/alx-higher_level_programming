123d3852209cd5e0a456fc5168fad2f5b4fc8256
ALI <ali012wkout@gmail.com>
2024-02-06 01:28:41 +0200

    task 12 answer

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,14 @@
+#!/user/bin/python3
+"""a module for task 12"""
+
+
+class MyInt(int):
+    """a subclass from int"""
+
+    def __eq__(self, __value: object):
+        """will behave as the != operator"""
+        return self != __value
+
+    def __ne__(self, __value: object):
+        """will behave as the == operator"""
+        return self == __value
