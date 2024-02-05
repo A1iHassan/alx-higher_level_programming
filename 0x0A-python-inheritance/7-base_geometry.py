61f6f79474a80cc2598e2e35c806dbe28e00962a
ALI <ali012wkout@gmail.com>
2024-02-06 00:11:59 +0200

    task 7 answer

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""a module for task 7
+"""
+
+
+class BaseGeometry:
+    """Doc"""
+
+    def area(self):
+        """Doc"""
+        raise Exception("area() is not implemented")
+    
+    def integer_validator(self, name, value):
+        """validates value"""
+        if type(value) != int:
+            raise TypeError("<name> must be an integer")
+        elif value <= 0:
+            raise ValueError("<name> must be greater than 0")
--- 0x0A-python-inheritance/test.py
+++ 0x0A-python-inheritance/test.py
@@ -1,13 +1,6 @@
 #!/usr/bin/python3
-MyList = __import__('1-my_list').MyList
 
-my_list = MyList()
-my_list.append(1)
-my_list.append(4)
-my_list.append(2)
-my_list.append(3)
-my_list.append(5)
-print(my_list)
-my_list.print_sorted()
-print(my_list)
-my_list.__class__.print_sorted()
+base = __import__('7-base_geometry').BaseGeometry
+
+b = base()
+b.integer_validator("name", -7)
