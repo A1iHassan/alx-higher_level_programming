34879c281b7419ac735bda85383732a10d3bdb10
ALI <ali012wkout@gmail.com>
2024-02-05 23:51:57 +0200

    task 6 answer

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""a module for task 6
+"""
+
+
+class BaseGeometry:
+    """Doc"""
+
+    def area(self):
+        """Doc"""
+        raise Exception("area() is not implemented")
