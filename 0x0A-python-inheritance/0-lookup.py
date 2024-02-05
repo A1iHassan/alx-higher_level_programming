fd35a199cc1bdec1f1071b17ed9865cf844b6ffc
ALI <ali012wkout@gmail.com>
2024-02-05 20:24:30 +0200

    created directory + task 0 answer

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""a module for task 0"""
+
+
+def lookup(obj):
+    """a function that returns a list of a
+    given object's attributes
+    """
+    return list(vars(obj).keys())
