f1353d8e13d28454d7ffcbe7d4913ec03f1e48e1
ALI <ali012wkout@gmail.com>
2024-02-05 23:41:02 +0200

    task 4 answer

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""a module for task 4"""
+
+
+def inherits_from(obj, a_class):
+    """checks if the object is an instance of a class that
+    inherited (directly or indirectly) from the specified class
+    """
+    return isinstance(obj, a_class) or issubclass(obj, a_class)
