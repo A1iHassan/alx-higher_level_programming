89773724be304d36b10dfa6c7d111044bafb91e4
ALI <ali012wkout@gmail.com>
2024-02-07 01:54:33 +0200

    task 10 answer

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -36,7 +36,5 @@ class Student:
         if attrs is None:
             return self.__dict__
         else:
-            for attr in attrs:
-                if not hasattr(self, attr):
-                    attrs.pop(attr)
-            return {attr: getattr(self, attr) for attr in attrs}
+            return {attr: getattr(self, attr) for attr
+                    in attrs if hasattr(self, attr)}
