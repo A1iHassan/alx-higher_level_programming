7d08adb2d951b3d7de237c7aa65e575a5f46fd3b
ALI <ali012wkout@gmail.com>
2024-02-07 01:46:30 +0200

    task 10 answer

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""a module for task 10
+
+contains a custom Class with all its attributes
+"""
+
+
+class Student:
+    """a class for student
+
+    Attributes:
+        first_name: student's first name
+        last_name: student's last name
+        age: student's age
+    """
+
+    def __init__(self, first_name, last_name, age):
+        """instantiates new Student instances
+
+        Args:
+            first_name: ...
+            last_name: ...
+            age: ...
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """retrieves a dictionary representation of
+        a Student instance
+
+        Return:
+            a type of dict
+        """
+        if attrs is None:
+            return self.__dict__
+        else:
+            return {attr: getattr(self, attr) for attr in attrs if hasattr(self, attr)}
