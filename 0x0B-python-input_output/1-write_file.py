a8b30aef87b42f039e463f77c4e3e52d5b52e356
ALI <ali012wkout@gmail.com>
2024-02-07 00:02:46 +0200

    task 1 edit

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -18,4 +18,4 @@ def write_file(filename="", text=""):
             number of characters written
     """
     with open(filename, 'w', encoding="utf-8") as f:
-        f.write(text)
+        return f.write(text)
