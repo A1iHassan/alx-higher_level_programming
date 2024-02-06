def88bc081ea315d5a7c079ed087ea38df1b580d
ALI <ali012wkout@gmail.com>
2024-02-07 00:28:20 +0200

    task 6 answer

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""a module for task 6
+
+a function that creates an Object from a 
+“JSON file”
+"""
+
+
+import json
+
+
+def load_from_json_file(filename):
+    """a function that creates an Object from a 
+    “JSON file”
+
+    Args:
+        filename: file to extract object from
+
+    Return:
+        Python object
+    """
+    with open(filename, 'r', encoding="utf-8") as f:
+        return json.load(f)
