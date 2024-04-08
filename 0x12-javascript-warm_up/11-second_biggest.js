747127092a46bd58fa53598fb565fad244d8c8e3
A1iHassan <ali012wkout@gmail.com>
2024-04-09 01:12:22 +0200

    commit by Ali Hassan

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -4,4 +4,8 @@ process.argv.splice(2).forEach(element => {
 	x.push(+element);
 });
 x.splice(x.indexOf(Math.max(...x)))
-console.log(Math.max(...x));
+if (x.length === 0 || x.length === 1) {
+	console.log(0);
+} else {
+	console.log(Math.max(...x));
+}
