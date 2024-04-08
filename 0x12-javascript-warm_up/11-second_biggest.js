4df9a60d6768b9264194bd841aec746e8e0c9bf9
A1iHassan <ali012wkout@gmail.com>
2024-04-09 01:06:46 +0200

    commit by Ali Hassan

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,4 +1,7 @@
 #!/usr/bin/node
-const x = process.argv;
-x.pop(Math.max(...x));
+const x = [];
+process.argv.splice(2).forEach(element => {
+  x.push(+element);
+});
+x.splice(x.indexOf(Math.max(...x)));
 console.log(Math.max(...x));
