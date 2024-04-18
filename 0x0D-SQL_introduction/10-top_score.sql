3a077ee28157a3a601ea451e89acfc79149a7bac
A1iHassan <ali012wkout@gmail.com>
2024-04-18 21:53:00 +0200

    commit by Ali Hassan

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,4 +1,4 @@
 -- answer for task 10
 
 -- lists name and score in descending order
-SELECT `name`, `score` FROM second_table ORDER BY `score` DESC;
+SELECT `score`, `name` FROM second_table ORDER BY `score` DESC;
