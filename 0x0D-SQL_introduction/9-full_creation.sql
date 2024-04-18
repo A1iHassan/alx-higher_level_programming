d9726e92e01b17bb92e016538aa6c07c868c9528
A1iHassan <ali012wkout@gmail.com>
2024-04-18 18:53:16 +0200

    commit by Ali Hassan

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -2,13 +2,13 @@
 
 -- creates a new table
 CREATE TABLE IF NOT EXISTS second_table (
-	id INT,
-	'name' VARCHAR(256),
-	score INT
+	`id` INT,
+	`name` VARCHAR(256),
+	`score` INT
 );
 
 -- add data to the table
-INSERT INTO second_table (id, name, score) VALUES
+INSERT INTO second_table (`id`, `name`, `score`) VALUES
 (1, 'Jhon', 10),
 (2, 'Alex', 3),
 (3, 'Bob', 14),
