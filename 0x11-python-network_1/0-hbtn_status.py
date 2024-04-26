5248f3208829074cd5939efb45c89840c8893f4d
A1iHassan <ali012wkout@gmail.com>
2024-04-26 18:55:10 +0300

    commit by Ali Hassan

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -5,8 +5,7 @@ url = 'https://alx-intranet.hbtn.io/status'
 
 with urllib.request.urlopen(url) as response:
     body = response.read()
-
-print("Body response:")
-print("    - type:", type(body))
-print("    - content:", body)
-print("    - utf8 content:", body.decode('utf-8'))
+    print("Body response:")
+    print(f"\t- type: {type(body)}")
+    print(f"\t- content: {body}")
+    print(f"\t- utf8 content: {body.decode('utf-8')}")
