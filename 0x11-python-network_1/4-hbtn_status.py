3787b991595330f484c29eb45035111690ebf856
A1iHassan <ali012wkout@gmail.com>
2024-04-27 01:19:47 +0300

    commit by Ali Hassan

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -10,4 +10,7 @@ if __name__ == "__main__":
     import requests
 
     response = requests.get("https://alx-intranet.hbtn.io/status")
+    print('Body response:')
+    print(f'\t- type: {type(response.text)}')
+    print(f'\t- content: {response.text}')
     print(response.text)
