c5e9b2d0901515c2557f868d9eea14bcc812bf5e
A1iHassan <ali012wkout@gmail.com>
2024-04-27 01:17:01 +0300

    commit by Ali Hassan

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+a module for task 4 answer
+
+a Python script that fetches https://alx-intranet.hbtn.io/status
+"""
+
+
+if __name__ == "__main__":
+    import requests
+
+    response = requests.get("https://alx-intranet.hbtn.io/status")
+    print(response.text)
