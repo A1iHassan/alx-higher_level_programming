7530f88c815c34a457e48466b47ae767250ce24b
A1iHassan <ali012wkout@gmail.com>
2024-04-26 19:21:33 +0300

    commit by Ali Hassan

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -2,15 +2,13 @@
 """
 a module for task 1 answer
 
-send a request to the URL and display the value of the X-Request-Id
+sends a request to the URL and displays the value of the X-Request-Id
 """
 
-import sys
-import urllib.request
+if __name__ == "__main__":
+    import sys
+    import urllib.request
 
-
-url = sys.argv[1]
-
-req = urllib.request.Request(url)
-with urllib.request.urlopen(req) as resp:
-	print(dict(resp.headers).get("X-Request-Id"))
+    url = sys.argv[1]
+    with urllib.request.urlopen(url) as response:
+        print(response.info().get('X-Request-Id'))
