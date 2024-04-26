86ccd984d35c6f0fee76198d498ffd479abcb6fa
A1iHassan <ali012wkout@gmail.com>
2024-04-27 02:23:31 +0300

    commit by Ali Hassan

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -2,3 +2,15 @@
 """
 a module for task 10 answer
 """
+
+
+if __name__ == '__main__':
+    import requests
+    from sys import argv
+
+    url = f"https://api.github.com/repos/{argv[2]}/{argv[1]}/commits"
+    response = requests.get(url)
+    commits = response.json()
+    for commit in commits[:10]:
+        print(commit.get('sha'), end=': ')
+        print(commit.get('commit').get('author').get('name'))
