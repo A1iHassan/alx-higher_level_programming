68c36f8972a75b3a4948c7b71d6c8c19130d5b25
A1iHassan <ali012wkout@gmail.com>
2024-04-27 02:19:24 +0300

    commit by Ali Hassan

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+a module for task 10 answer
+"""
+
+
+if __name__ == '__main__':
+    import requests
+    from requests.auth import HTTPBasicAuth
+    from sys import argv
+
+    url = 'https://api.github.com/users/{}'.format(argv[1])
+    r = requests.get(url,
+                     auth=HTTPBasicAuth(argv[1], argv[2]))
+    print(r.json().get('id'))
