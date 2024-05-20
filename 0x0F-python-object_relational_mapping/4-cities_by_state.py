90707455737df56e29165bad5a13a5aef39c936b
A1iHassan <ali012wkout@gmail.com>
2024-05-21 01:04:28 +0300

    a commit by Ali Hassan

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -20,7 +20,10 @@ if __name__ == '__main__':
     )
 
     cursor = database.cursor()
-    cursor.execute(f'SELECT * FROM cities ORDER BY id ASC;')
+    cursor.execute("""SELECT cities.id, cities.name, states.name
+                   FROM cities
+                   JOIN states ON cities.state_id = states.id
+                   ORDER BY cities.id ASC;""")
     results = cursor.fetchall()
     for line in results:
         print(line)
