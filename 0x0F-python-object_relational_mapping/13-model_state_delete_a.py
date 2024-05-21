92c2f8f6ed921ec9a0510285ae65f8b63ac01898
A1iHassan <ali012wkout@gmail.com>
2024-05-22 00:25:46 +0300

    a commit by Ali Hassan

---
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+task 13 answer
+"""
+
+
+if __name__ == '__main__':
+    from sqlalchemy.orm import sessionmaker
+    from sqlalchemy import create_engine
+    from model_state import State, Base
+    from sys import argv
+
+    user = argv[1]
+    password = argv[2]
+    db = argv[3]
+
+    engine = create_engine(
+        f'mysql+mysqldb://{user}:{password}@localhost:3306/{db}')
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    Base.metadata.create_all(engine)
+    try:
+        state_to_delete = session.query(
+            State).filter(State.id == 2).one_or_none()
+        if state_to_delete:
+            session.delete(state_to_delete)
+            session.commit()
+    except Exception as e:
+        session.rollback()
+    finally:
+        session.close()
