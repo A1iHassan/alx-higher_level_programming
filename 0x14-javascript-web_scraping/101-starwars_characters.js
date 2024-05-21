a279c826091cd799795ad0c3a7e8cf5ef7b99d99
A1iHassan <ali012wkout@gmail.com>
2024-05-21 20:53:11 +0300

    a commit by Ali Hassan

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,19 +1,28 @@
 #!/usr/bin/node
-const request = require('request');
-const url = 'https://swapi-api.alx-tools.com/api/films/';
-request(`${url}${process.argv[2]}`, (err, res, data) => {
+const request = require("request");
+const url = "https://swapi-api.alx-tools.com/api/films/";
+
+function search(url) {
+  return new Promise((resolve, reject) => {
+    request(url, (err, res, data) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(JSON.parse(data).name);
+      }
+    });
+  });
+}
+
+request(`${url}${process.argv[2]}`, async (err, res, data) => {
   if (err) {
     console.log(err);
   } else {
     const characters = JSON.parse(data).characters;
-    characters.forEach((element, index) => {
-      request(element, (err2, res2, data2) => {
-        if (err2) {
-          console.log(err2);
-        } else {
-          console.log(JSON.parse(data2).name);
-        }
-      });
-    });
+    const promises = characters.map((character) => search(character));
+    const names = await Promise.all(promises);
+    for (let name of names) {
+      console.log(name);
+    }
   }
 });
