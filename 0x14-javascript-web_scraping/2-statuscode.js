904958eb8b511da9407407104705a15f0b2ff27c
A1iHassan <ali012wkout@gmail.com>
2024-05-21 18:24:29 +0300

    a commit by Ali Hassan

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -4,6 +4,6 @@ request(process.argv[2], (err, response, body) => {
   if (err) {
     console.log(err);
   } else {
-    console.log('code: ', response.statusCode);
+    console.log('code:', response.statusCode);
   }
 });
