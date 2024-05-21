8d667c39afe93e2f9387aa497a3544a6bd029339
A1iHassan <ali012wkout@gmail.com>
2024-05-22 00:50:37 +0300

    a commit by Ali Hassan

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -20,7 +20,7 @@ if __name__ == '__main__':
     Session = sessionmaker(bind=engine)
     session = Session()
     Base.metadata.create_all(engine)
-    cities = session.query(City).order_by(City.id).all()
-    for city in cities:
-        print(f"{city.state.name}: ({city.id}city.state.) {city.name}")
+    city = session.query(State, City).join(City).order_by(City.id)
+    for state, city in city:
+        print("{}: ({}) {}".format(state.name, city.id, city.name))
     session.close()
